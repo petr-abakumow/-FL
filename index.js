0d51ec6ddc587c0388a30cf61c57e5af5297ebff
Petr Abakumov <petr-abakumow@yandex.ru>
2021-03-02 14:34:51 +0300

    feat: mobile

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('Привет, мир!');
+
+const popupOpenCloseButton = document.querySelector('.menu-mobile-icon');
+const popupOpenedImage = document.querySelector('.menu-mobile-icon__image');
+const popupClosedImage = document.querySelector('.popup-close-icon');
+const popup = document.querySelector('.popup-menu');
+
+
+popupOpenCloseButton.addEventListener('click', () => {
+    if (popup.classList.contains('display-flex') === false) {
+        popup.classList.add('display-flex');
+        popupOpenedImage.classList.add('display-none');
+        popupClosedImage.classList.add('display-flex');
+        console.log('работает')
+    } else {
+        popup.classList.remove('display-flex');
+        popupOpenedImage.classList.remove('display-none');
+        popupClosedImage.classList.remove('display-flex');
+        console.log('не работает')
+    }
+})
+
